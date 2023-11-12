a7a5dfd0deadf9f86b3128db5568719ca327a918
Aamir Khan <oneaamirkhan@icloud.com>
2023-11-12 23:09:42 +0530

    initial setup

---
+++ News_App/News_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  News_AppApp.swift
+//  News_App
+//
+//  Created by عامر خان on 18/10/23.
+//
+
+import SwiftUI
+
+@main
+struct News_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
